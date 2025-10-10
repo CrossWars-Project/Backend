1ca7edfae04202c87a2bf1315545a77aadb3329e
Danielle <dmaddo@uw.edu>
2025-10-10 16:09:31 -0700

    Support communication with frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from app.db import supabase
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
