f5a0ffdc88ffa047223b795e74f2e780a00e46ef
jackiecosa <jackie@benincosa.com>
2025-10-27 12:07:52 -0700

    linting

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -5,18 +5,15 @@ import uuid
 
 client = TestClient(app)
 
-MOCK_USER = {
-    "id": str(uuid.uuid4()),
-    "display_name": "Test User 1"
-}
+MOCK_USER = {"id": str(uuid.uuid4()), "display_name": "Test User 1"}
 
 
 def test_create_user_stats_creates_entry(monkeypatch):
     """
-    Test that create user stats posts a new entry to the stats table in DB and that 
+    Test that create user stats posts a new entry to the stats table in DB and that
     the data is retrieved as expected (display name and user id match, stats are default stats)
     """
-    #ensure successful post
+    # ensure successful post
     res = client.post("/stats/create_user_stats", json=MOCK_USER)
     assert res.status_code == 200
     data = res.json()
