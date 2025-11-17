cef8d2f87ab4df10ac83dca4836c83ce7b44dad3
jackiecosa <jackie@benincosa.com>
2025-11-16 20:49:51 -0800

    allow live url as origin

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://crosswars-988ycwmvo-jacquis-projects-6689649a.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
