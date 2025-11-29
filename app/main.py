e35f4ba712761db85026145ba254d46763b90dcf
Danielle <dmaddo@uw.edu>
2025-11-29 12:48:35 -0800

    update cors to allow run preview and crosswars.xyz

--- app/main.py
+++ app/main.py
@@ -14,7 +14,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://www.crosswars.xyz/",
+        "http://localhost:4173",      # npm run preview
+        "https://crosswars.xyz",      # No trailing slash
+        "https://www.crosswars.xyz",  # Support both www and non-www
     ],
     allow_credentials=True,
     allow_methods=["*"],
